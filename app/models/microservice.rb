9018f5914996197fc074023f51a3d3974754b9b1
balfoldi <balfoldi@yahoo.fr>
2020-09-08 18:44:41 +0200

    donated_sum relations

--- app/models/microservice.rb
+++ app/models/microservice.rb
@@ -4,7 +4,9 @@ class Microservice < ApplicationRecord
     numericality: true,
     numericality: { greater_than: 0.99e2, less_than: 0.501e3}
 
+    belongs_to :manufacturer, class_name: "Creatrix"
     belongs_to :microservice_category
     has_many :achievments
+    has_many :fundraisers
 
 end

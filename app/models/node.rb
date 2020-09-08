93de5e2c63d5ac0e428ea750242a092fcdb84b98
balfoldi <balfoldi@yahoo.fr>
2020-09-08 15:13:27 +0200

    node relations

--- app/models/node.rb
+++ app/models/node.rb
@@ -1,2 +1,5 @@
 class Node < ApplicationRecord
+    belongs_to :nodeable, polymorphic: true
+    belongs_to :creatrix, polymorphic: true
+    has_many :achievement, through: :node_achievement_relations
 end

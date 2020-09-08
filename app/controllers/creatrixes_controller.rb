2cb692f85ff14938377037f1b4a7b355e456a25a
Estebandelaciudad <emeran.gainville@gmail.com>
2020-09-08 11:58:34 +0200

    creatrix show & controller and seed added

---
+++ app/controllers/creatrixes_controller.rb
@@ -0,0 +1,4 @@
+class CreatrixesController < ApplicationController
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'creatrixes/show'
   devise_for :creatrixes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

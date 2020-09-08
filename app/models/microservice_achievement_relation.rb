4c0b920f5fd2365ac0e6aee3d47e51bbb9e1211e
balfoldi <balfoldi@yahoo.fr>
2020-09-08 19:07:07 +0200

    NodAchievementRelation

--- app/models/microservice_achievement_relation.rb
+++ app/models/microservice_achievement_relation.rb
@@ -1,4 +1,4 @@
 class MicroserviceAchievementRelation < ApplicationRecord
-    belongs_to :achievment
+    belongs_to :achievement
     belongs_to :microservice
 end
--- db/migrate/20200908164911_create_microservice_achievement_relations.rb
+++ db/migrate/20200908164911_create_microservice_achievement_relations.rb
@@ -1,7 +1,7 @@
 class CreateMicroserviceAchievementRelations < ActiveRecord::Migration[5.2]
   def change
     create_table :microservice_achievement_relations do |t|
-      t.belongs_to :achievment, index: true
+      t.belongs_to :achievement, index: true
       t.belongs_to :microservice, index: true
       t.timestamps
     end

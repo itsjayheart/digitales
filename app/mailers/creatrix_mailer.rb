a03830c77917afde13861c3fd422fdb6c5aff8df
balfoldi <balfoldi@yahoo.fr>
2020-09-08 19:42:51 +0200

    mailer mail files created, welcome email operational

--- app/mailers/creatrix_mailer.rb
+++ app/mailers/creatrix_mailer.rb
@@ -1,15 +1,17 @@
 class CreatrixMailer < ApplicationMailer
   default from: 'balfoldi@yahoo.fr'
 
-  #UserMailer.creatrix_micro_service_request(creatrix).deliver_now
   def creatrix_micro_service_request(creatrix)
     mail(to: @creatrix.email, subject: 'micro service request') 
-
   end
 
   def creatrix_micro_service_accepted(creatrix)
     mail(to: @creatrix.email, subject: 'micro service accepted') 
 
+  end
+
+  def creatrix_welcome(creatrix)
+    mail(to: @creatrix.email, subject: 'welcome') 
   end
 
 end

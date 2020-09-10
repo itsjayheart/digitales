28a1e800ce001842b1fd28f9a5531750edeb7886
Lucas <lucas.gautier@live.fr>
2020-09-10 11:33:40 +0200

    RESPONSIVE_MANSORY_GRID_V2

--- app/controllers/nodes_controller.rb
+++ app/controllers/nodes_controller.rb
@@ -1,7 +1,7 @@
 class NodesController < ApplicationController
     def index
       @nodes = Node.all
-      @cards_classes = ["card text-white bg-dark mb-3", "card bg-light mb-3", "card-columns"]
+      @cards_classes = ["card text-white bg-dark mb-3", "card bg-light mb-3", "card-columns",]
     end
 
     def show

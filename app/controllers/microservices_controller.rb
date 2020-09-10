674e09ac4aa594b41cef118fb75e120fc86ae3ea
balfoldi <balfoldi@yahoo.fr>
2020-09-10 19:18:03 +0200

    alert + jax working

--- app/controllers/microservices_controller.rb
+++ app/controllers/microservices_controller.rb
@@ -5,7 +5,7 @@ class MicroservicesController < ApplicationController
   	@microservice = Microservice.new(creatrix: current_creatrix, microservice_category: MicroserviceCategory.find(params['microservice_category_id']), price: price)
     @microservice_category = @microservice.microservice_category
 
-    @microservice.save ? @errors = nul : @errors = @microservice.errors.full_messages.to_sentence
+    @microservice.save ? @errors = nil : @errors = @microservice.errors.full_messages.to_sentence
     
     respond_to do |format|
       format.html { redirect_to creatrix_path(current_creatrix.id) }

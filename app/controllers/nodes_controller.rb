e67eff33516f546de8d4483434260d53d92035fe
itsjayheart <wearethetrueservants@gmail.com>
2020-09-13 01:22:52 +0200

    DRYing up nodes controller

--- app/controllers/nodes_controller.rb
+++ app/controllers/nodes_controller.rb
@@ -8,62 +8,13 @@ class NodesController < ApplicationController
     def show
       @donated_sum = DonatedSum.new
 
-      @node = Node.find(params[:id])    
-      if Fundraiser.find_by(microservice_request: @node.achievements.last.microservice_requests.last)
+      @node = Node.find(params[:id])
+
+      if Fundraiser.find_by(microservice_request: @node.achievements.last.microservice_requests.last) #if node.pending_microservice_request.accepted?
         @fundraiser = Fundraiser.find_by(microservice_request: @node.achievements.last.microservice_requests.last)
-        @fundraiser.funded? ? @fund_status = "The goal is met" : @fund_status = "funding in progress"
+        #@fundraiser.funded? ? @fund_status = "The goal is met!" : @fund_status = "funding in progress"
         @per_cent_status = (@fundraiser.current_fundings.ceil * 100) / @fundraiser.goal.ceil
       end
-      
-      #$$$$$$$$$$$$$$$$$$$$$---NODE_DATA_HASH---$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
-      @achievements_node = Hash.new
-      @node.achievements.each do |achievment| 
-        achievement_microservices = []
-        achievment.microservices.each { |microservice| achievement_microservices << microservice.microservice_category.name}
-        @achievements_node[achievment.achievement_category.name] = achievement_microservices
-      end  
-      #$$$$$$$$$$$$$$$$$$$$$---NODE_DATA_HASH---$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
-
-      #$$$$$$$$$$$$$$$$$$$$$---STATICS_DATA_ARRAYS---$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
-        #achievements
-        achievements = ["CREATIVE WRITING", "2D DIGITAL ART", "3D DIGITAL ART", "ACTING", "EDITING", "COMPOSING"]
-        #micro_services
-        wri_micros = ["1.5k synopsis", "10k words one-shot", "10k words one-shot: screenplay translation"]
-        flat_micros = ["2D character design", "2D prop design", "50-150 panels webcomic : thumbnail", "50-150 panels webcomic :sketching", "50-150 panels webcomic : lineart", "50-150 panels webcomic : flats", "50-150 panels webcomic : shadows and highlights", "50-150 panels webcomic : backgrounds", "50-150 panels webcomic : cover"]          
-        volume_micros = ["3D modeling : character design", "3D modeling : prop design", "3D scenery artwork", "3D character artwork", "3D rigging of a character", "1-3 minutes animated scene"]          
-        act_micros = ["1-3 minutes animated scene voice acting"]          
-        editing_micros = ["1-3 minutes animated scene editing"]          
-        music_micros = ["1-3 minutes music theme"] 
-        microservices = [wri_micros, flat_micros, volume_micros, act_micros, editing_micros, music_micros]
-      #$$$$$$$$$$$$$$$$$$$$$---STATICS_DATA_ARRAYS---$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
-      
-      @achievments_status = Hash.new
-      (achievements.length-1).times do |i| 
-        if @achievements_node.keys.include?(achievements[i]) 
-          (microservices[i].length).times do |x| 
-            if @achievements_node[achievements[i]].include?(microservices[i][x])
-              microservices[i][x] = [microservices[i][x], "text-primary"]
-            else
-              microservices[i][x] = [microservices[i][x], "text-secondary"]
-            end
-          end
-        else
-          (microservices[i].length).times do |x| 
-            microservices[i][x] = [microservices[i][x], "text-secondary"]
-          end
-        end
-        @achievments_status[achievements[i]] = microservices[i]
-      end
-      
-      #achievements
-      @status_achievement = ["text-secondary"]
-      puts ("%"*100)
-      puts @achievments_status
-      puts ("%"*100)
-      puts ("$"*100)
-      puts @achievements_node
-      puts ("$"*100)
-      #microservices
 
     end
 

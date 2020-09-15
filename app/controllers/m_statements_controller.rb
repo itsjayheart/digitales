39badf8c1ec0faba583d6d02f0fdee75a27bf54b
itsjayheart <wearethetrueservants@gmail.com>
2020-09-15 16:03:03 +0200

    completing digitale creation form

--- app/controllers/m_statements_controller.rb
+++ app/controllers/m_statements_controller.rb
@@ -2,7 +2,7 @@ class MStatementsController < ApplicationController
 	def create
 		@digitale = Digitale.find(params['digitale_id'])
 		@m_statement = MStatement.create(creatrix: current_creatrix, sentence: params['content'], description: params['description'])
-		@node = Node.new(nodeable: @m_statement, creatrix: current_creatrix, digitale: @digitale, sentence: params['content'], description: params['description'])
+		@node = Node.new(nodeable: @m_statement, creatrix: current_creatrix, digitale: @digitale, title: params['content'], content: params['description'])
 		
 		if @node.save
 			if @digitale.m_statements
@@ -11,9 +11,8 @@ class MStatementsController < ApplicationController
 						format.js { }
 					end
 				end
-			else
-				redirect_to 
-
+			end
+		end
 	end
 
 	def update

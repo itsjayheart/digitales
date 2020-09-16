d656c2fc67e6cc7030360d78214b511575e2a82b
BorisSIUS <balfooldi@yahoo.fr>
2020-09-16 12:35:53 +0200

    push

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,9 +14,9 @@ class ApplicationController < ActionController::Base
     protected
 
       def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password)}
+        devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:hexanyme, :email, :password)}
 
-        devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password)}
+        devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:hexanyme, :email, :password, :current_password)}
       end
 
 end
